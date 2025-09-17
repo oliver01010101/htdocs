6a88bddb6a288737aa0f0a46c756b90e962672e6
VargaO <2006vargaoliver@gmail.com>
2025-09-17 13:06:07 +0200

    cmmit all

--- hazi20250917/osszeadotabla.php
+++ hazi20250917/osszeadotabla.php
@@ -1,1 +1,34 @@
+<style>
+    table,td,th {
+        border: 1px solid #999;
+        border-collapse: collapse;
+        padding: 4px;
+        text-align: center;
+    }
+</style>
+
 <?php
+$size = 10;
+?>
+<table>
+    <tr>
+        <th>+</th>
+        <?php for($j = 1; $j <= $size; $j++): ?>
+            <th>
+                <?= $j ?>
+            </th>
+        <?php endfor; ?>
+    </tr>
+    <?php for($row = 1; $row <= $size; $row++): ?>
+        <tr>
+            <th> <?= $row ?> </th>
+
+            <?php for($col = 1; $col <= $size; $col++): ?>
+                <td>
+                    <?= $row + $col; ?>
+                </td>
+            <?php endfor; ?>
+        </tr>
+    <?php endfor; ?>
+
+</table>
