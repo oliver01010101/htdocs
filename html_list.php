4c4881711257c63c46107179923dc6953b70b6f1
VargaO <2006vargaoliver@gmail.com>
2025-09-17 11:50:23 +0200

    commit all

--- html_list.php
+++ html_list.php
@@ -23,3 +23,22 @@ echo "</ul>";
 
 
 ?>
+
+<h3>for html-php</h3>
+<ul>
+    <?php for ($i = 0; $i < count($fruits); $i++): ?>
+        <li>
+            <?= $fruits[$i] ?>
+        </li>
+    <?php endfor; ?>
+</ul>
+
+
+<h3>for html-php</h3>
+<ul>
+    <?php foreach ($fruits as $fruit): ?>
+        <li>
+            <?= $fruit ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
