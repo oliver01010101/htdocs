7463a79960871ac6df94849fcf75e349aecb863e
VargaO <2006vargaoliver@gmail.com>
2025-09-17 13:03:34 +0200

    cmmit all

---
+++ hazi20250917/nav.php
@@ -0,0 +1,14 @@
+<?php
+$menu_items = ["Home", "Products", "About", "Contact"];
+
+
+?>
+
+<ul>
+    <li>
+        <a href="#Főoldal">Főoldal</a>
+    </li>
+    <li>
+        <a href="#itemname">Főoldal</a>
+    </li>
+</ul>
